c895ea3e38baf953aa13617facac8175f13e8932
Shohel Al Mahmud <shohel@Shohels-iMac-2.fritz.box>
2025-02-17 23:11:51 +0100

    port and host has been added in server.py.

--- server.py
+++ server.py
@@ -44,4 +44,4 @@ def favicon():
     return send_from_directory('static', 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run(port=8000,host='0.0.0.0')
